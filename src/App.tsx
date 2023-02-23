e17e8e15000441e1c014b9845b3c4747f978d8dd
Gabriel Oliveira <gboliveira2503@gmail.com>
2023-02-23 14:00:02 -0300

    wip: create modal base

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
 import './App.css'
+import { useState } from "react"
+import Leilao from './components/Leilao';
+import { ModalBase } from './components/ModalBase'
 import CarList from './components/ProductCard';
 import Global from "./styles/global";
 
-function App() {
+import { motion, AnimatePresence } from "framer-motion"
 
+
+
+function App() {
+  const [ modalOpen, setModalOpen ] = useState(false)
+  const close = () => setModalOpen(false)
+  const open = () => setModalOpen(true)
+
+  
+
+  
   return (
     <div className="App">
+     
       <Global />
+      <button onClick={() => (modalOpen ? close() : open() )}>Abrir</button>
+      {
+        modalOpen && <ModalBase handClose={close()}  titleHeader="olá word">
+        <button>confia</button>
+        </ModalBase>
+      }
+     
+      
       
     </div>
   )
---
+++ src/components/ModalBase/styles.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+
+export const StyledModal = styled.div`
+    height: 24.1875em;
+    width: clamp(50%, 21.5em, 97.5%);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    position: relative;
+
+    border: 1px solid red;
+
+
+`
