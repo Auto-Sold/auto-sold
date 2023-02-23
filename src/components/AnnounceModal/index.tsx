b361bf856883f6c486df81163516df759f07a1e5
Marchi8 <luandeoliveirasoares@gmail.com>
2023-02-23 14:38:53 -0300

    fix: now backdrop component can receive any setstate

--- src/components/AnnounceModal/index.tsx
+++ src/components/AnnounceModal/index.tsx
@@ -1,9 +1,13 @@
 import { motion } from "framer-motion"
 import { useContext } from "react"
 import { AnnounceContext } from "../../contexts/AnnounceContext"
+import { useForm } from "react-hook-form"
+import { yupResolver } from "@hookform/resolvers/yup"
 import BackDrop from "../BackDrop"
+import * as yup from "yup"
+import { IAnnounceData } from "../../interfaces"
 
-const dropIn = {
+export const dropIn = {
     hidden: {
         y: "-100vh",
         opacity: 0,
@@ -25,10 +29,32 @@ const dropIn = {
 }
 
 const AnnounceModal = () => {
-    const { setAnnounceModal } = useContext(AnnounceContext)
+    const { postAnnouncement, setAnnounceModal } = useContext(AnnounceContext)
+
+    const dados = {
+        "announceType": "string2",
+        "title": "string2",
+        "year": "string2",
+        "km": 50,
+        "price": 20,
+        "description": "string2",
+        "vehicleType": "string2",
+        "image": "string2"
+    }
+
+    const formSchema = yup.object().shape({
+        // announceType: yup.string().required("Telefone necessário"),
+        // title: yup.string().required("Telefone necessário"),
+        // year: yup.string().required("Nome necessário")
+    })
+
+    const { register, handleSubmit, formState: { errors } } =
+        useForm<IAnnounceData>({
+            resolver: yupResolver(formSchema)
+        })
 
     return (
-        <BackDrop>
+        <BackDrop setState={setAnnounceModal}>
             <motion.div
                 className="announce-modal"
                 onClick={(e) => e.stopPropagation()}
@@ -37,11 +63,17 @@ const AnnounceModal = () => {
                 animate="visible"
                 exit="exit"
             >
-                <h1>testeeeee</h1>
-                <h1>testeeeee</h1>
-                <h1>testeeeee</h1>
-                <h1>testeeeee</h1>
-                <h1>testeeeee</h1>
+                <button onClick={
+                    () => setAnnounceModal(false)}>X</button>
+                <form onSubmit={handleSubmit(postAnnouncement)}>
+                    <h1>testeeeee</h1>
+                    <h1>testeeeee</h1>
+                    <h1>testeeeee</h1>
+                    <h1>testeeeee</h1>
+                    <h1>testeeeee</h1>
+                    <button type="submit">Criar</button>
+                </form>
+                {/* <button onClick={() => postAnnouncement(dados)}>aaaa</button> */}
             </motion.div>
         </BackDrop>
     )
--- src/contexts/AnnounceContext.tsx
+++ src/contexts/AnnounceContext.tsx
@@ -6,16 +6,18 @@ import {
     useContext,
     useState
 } from "react";
+import { API } from "../api";
+import { IAnnounceData } from "../interfaces";
 
 export interface IAnnounceAuth {
     announceModal: boolean;
     setAnnounceModal: Dispatch<SetStateAction<boolean>>;
+    postAnnouncement: (data: IAnnounceData) => void;
     // editCard: string | null;
     // friendIdState: string | undefined;
     // setEditCard: Dispatch<SetStateAction<string | null>>;
     // setfriendIdState: Dispatch<SetStateAction<string | undefined>>;
     // addFriend: (email: string, name: string, phone: string) => void;
-    // createFriend: (data: IAddFriendData) => void;
     // removeFriend: (data: string) => void;
     // editPhone: (data: any) => void;
 }
@@ -29,8 +31,19 @@ export const AnnounceContext = createContext<IAnnounceAuth>({} as IAnnounceAuth)
 function AnnounceProvider({ children }: IAnnounceProps) {
     const [announceModal, setAnnounceModal] = useState<boolean>(false)
 
+    const postAnnouncement = (data: IAnnounceData) => {
+        console.log(data)
+        API.post(`/announce`, data)
+            .then(res => { console.log(res) })
+            .catch(err => { console.log(err.response.data.message) })
+    }
+
     return (
-        <AnnounceContext.Provider value={{ announceModal, setAnnounceModal }}>
+        <AnnounceContext.Provider value={{
+            announceModal,
+            setAnnounceModal,
+            postAnnouncement
+        }}>
             {children}
         </AnnounceContext.Provider>
     )
